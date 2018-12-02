f199fce6610468242b2b9ae7ad9e20de63fa8163
Owner <Owner@DESKTOP-FECS9BK>
2018-12-02 19:36:41 +0200

    Controllers

---
+++ SnowBoardingProject_Core/Controllers/CompanyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SnowBoardingProject_Core.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Company()
+        {
+            return View();
+        }
+    }
+}
---
+++ SnowBoardingProject_Core/Controllers/ExperienceController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SnowBoardingProject_Core.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        public IActionResult Experiance()
+        {
+            return View();
+        }
+    }
+}
